942d4148be034e9c5567b8054e72f3ee50af6310
Allan Espinosa <allan.espinosa@outlook.com>
2016-07-17 01:28:43 -0500

    Provision the graphite stack automatically

---
+++ demo/graphite.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Carbon
+apt-get update
+apt-get --no-install-recommends install -y graphite-carbon
+systemctl start carbon-cache.service
+
+# Graphite-Web
+apt-get --no-install-recommends install -y \
+    graphite-web apache2 libapache2-mod-wsgi
+
+ln -sf /usr/share/graphite-web/apache2-graphite.conf \
+    /etc/apache2/sites-available/100-graphite.conf
+
+a2dissite 000-default && a2ensite 100-graphite 
+
+sudo -u _graphite graphite-manage syncdb --noinput 
+
+systemctl restart apache2.service
