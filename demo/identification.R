c9ae029aa598930caa48d406ad055eda8d91d037
Allan Espinosa <allan.espinosa@outlook.com>
2016-07-18 18:03:59 -0500

    Model the system using least-squares regression

---
+++ demo/identification.R
@@ -0,0 +1,14 @@
+install.packages('reshape')
+library(reshape)
+
+raw<-read.csv("data-model.raw", header=FALSE, sep = " ")
+names(raw)<-c("var", "value", "time")
+names(data)<-c("time", "y", "u")
+N<-nrow(data)-1
+mean_u<-mean(data[1:N,]$u)
+mean_y<-mean(data[2:(N+1),]$y)
+rms_data<-data.frame(time = data[1:N,]$time, uk = data[1:N,]$u, yk = data[1:N,]$y, yk1 = data[2:(N+1),]$y)
+
+model<-lm(yk1 ~ yk + uk, rms_data)
+
+plot(rms_data$uk + mean_u, rms_data$yk1 + mean_y, xlab = "Number of Replicas", ylab="CPU Utilization")
