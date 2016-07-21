4c460ad33e0dd97875ffd3d0a9f5e789ca46fa77
Allan Espinosa <allan.espinosa@outlook.com>
2016-07-21 12:04:18 -0500

    Bang head to get PI control to work

--- demo/main.rb
+++ demo/main.rb
@@ -5,21 +5,25 @@ sensor = ControlTheory::Sensor.new
 actuator = ControlTheory::Actuator.new
 
 class Controller
-  def initialize(target = 0.73603, k_p=-97.736, operating_point = 69.362)
+  def initialize(target = 0.73603, k_p=-97.736, k_i=0.0,operating_point = 69.362)
     @k_p = k_p
+    @k_i = k_i
     @target = target.to_f
     @operating_point = operating_point
+    @total_error = 0.0
   end
 
   def work(actual)
     error = @target - actual
-    puts "Error #{@target} - #{actual} = #{error}"
+    puts "Error #{error}"
+    @total_error += error
 
-    (@k_p * error + @operating_point).ceil
+    (@k_p * error + @k_i * @total_error + @operating_point).ceil
   end
 end
 
-controller = Controller.new 0.7, -96.0
+#controller = Controller.new 0.7, -96.0
+controller = Controller.new 0.7, -96.0, -0.5
 
 require 'socket'
 
