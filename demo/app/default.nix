92173d6054a44b76af30d5b54be2db170cc6b6a7
Allan Espinosa <allan.espinosa@outlook.com>
2016-10-31 00:13:20 -0500

    Restructure to work with distributed builds
    * Allow nixpkgs.system to be overridable
    * Comment how to run distributed builds

--- demo/app/default.nix
+++ demo/app/default.nix
@@ -1,9 +1,23 @@
-with import <nixpkgs> {};
-
+# To build on macOS
+# NIX_CURRENT_LOAD=`pwd` NIX_REMOTE_SYSTEMS=`pwd`/remotes.conf \
+# NIX_BUILD_HOOK=$HOME/.nix-profile/libexec/nix/build-remote.pl \
+# nix-build  --argstr system x86_64-linux
+#
+# remotes.conf
+# vagrant@<ip> x86_64-linux $HOME/.vagrant.sh/insecure_private_key 1
+{ system ? builtins.currentSystem }:
+with import <nixpkgs> {
+  system = system;
+};
 
 stdenv.mkDerivation {
   name = "cpu-app";
+  enableSharedExecutables = false;
   buildInputs = [
     (haskellPackages.ghcWithPackages (p: [p.yesod ]))
   ];
+
+  src = ./.;
+
+  phases = [ "unpackPhase" "buildPhase" "installPhase" "fixupPhase" ];
 }
